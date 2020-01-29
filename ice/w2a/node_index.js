def6625e641d7f175c73684f5e90e1c55232cc3d
Kiara Chacon <ki996093@ucf.edu>
2020-01-29 09:04:26 -0500

    updated node

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,4 +1,7 @@
-function scopeDemo() {
+console.log("Hello World");
+var x = 1;
+//var y = [1];
+function scopeDemo(y1) {
     console.log(x)
     let a = 3, b = 4, c = 5;
     console.log(a);
@@ -8,4 +11,7 @@ function scopeDemo() {
     if(true) {
         var x = 0;
     }
+    return a;
 }
+console.log(scopeDemo(y)+10);
+console.log(y);
