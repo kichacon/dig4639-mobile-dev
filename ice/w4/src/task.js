a4be671544e2b460bd2e64c85e54419bb5609a5f
Kiara Chacon <ki996093@ucf.edu>
2020-02-12 10:21:28 -0500

    eslint changes

--- ice/w4/src/task.js
+++ ice/w4/src/task.js
@@ -1,18 +1,17 @@
 class Task {
-    constructor(props) {
-        this.props = props;
-        this.element = document.createElement("div");
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        this.element.appendChild(checkbox);
-        let span = document.createElement("span");
-        this. element.appendChild(span);
-        span.innerHTML = this.props.content;
-        
-    }
-    render() {
-        return this.element;
-    }
+  constructor (props) {
+    this.props = props
+    this.element = document.createElement('div')
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    this.element.appendChild(checkbox)
+    const span = document.createElement('span')
+    this.element.appendChild(span)
+    span.innerHTML = this.props.content
+  }
 
+  render () {
+    return this.element
+  }
 }
-export default Task;
+export default Task
